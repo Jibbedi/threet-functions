7f6717f7e56f204cc624649e23ceca18212f888e
Johannes Kling <johannes.kling92@gmail.com>
2018-07-21 21:18:02 +0200

    added constants for collections

---
+++ functions/src/constants/collections.ts
@@ -0,0 +1,6 @@
+export const TOURNAMENT_COLLECTION = 'tournaments';
+export const PLAYER_COLLECTION = 'players';
+export const GAME_COLLECTION = 'games';
+
+
+export const ALL_COLLECTIONS = [TOURNAMENT_COLLECTION, PLAYER_COLLECTION, GAME_COLLECTION];
--- functions/src/helper/database.ts
+++ functions/src/helper/database.ts
@@ -1,11 +1,12 @@
 import * as admin from 'firebase-admin';
 import {Player} from '../types/Player';
 import {Game} from '../types/Game';
+import {GAME_COLLECTION, PLAYER_COLLECTION, TOURNAMENT_COLLECTION} from '../constants/collections';
 
 export function getPlayerById(id: string, stage: string): Promise<Player> {
     return admin
         .firestore()
-        .collection(stage + 'players')
+        .collection(stage + PLAYER_COLLECTION)
         .doc(id)
         .get()
         .then(snapshot => snapshot.data() as Player);
@@ -18,7 +19,7 @@ export function getPlayersForIds(ids: string[], stage): Promise<Player[]> {
 export function writeGameToDatabase(game: Game, stage: string): Promise<string> {
     return admin
         .firestore()
-        .collection(stage + 'games')
+        .collection(stage + GAME_COLLECTION)
         .add(game)
         .then(gameWrite => gameWrite.id);
 }
@@ -26,7 +27,7 @@ export function writeGameToDatabase(game: Game, stage: string): Promise<string> 
 export function updateTournamentWithGameIds(tournamentId: string, gameIds: string[], stage: string) {
     return admin
         .firestore()
-        .collection(stage + 'tournaments')
+        .collection(stage + TOURNAMENT_COLLECTION)
         .doc(tournamentId)
         .update({games: gameIds})
 }
